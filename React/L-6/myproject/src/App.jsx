0a7f73cd3059d46dc204bd459f1a22780a3dc6ec
vijay dankhara <vijaydankhara111@gmail.com>
2024-07-10 22:40:11 +0530

    10-07-2024

---
+++ React/L-6/myproject/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+      </>
+  )
+}
+
+export default App
---
+++ React/L-6/myproject/src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from './Navbar.module.css'
+import { useState } from "react";
+
+const Navbar = () => {
+
+    let initialState = 0
+
+    const [count , setCount] = useState(initialState)
+
+    const box =
+        [
+            {
+                height: "100px",
+                width: "200px"
+            },
+            {
+                height: "100px",
+                width: "200px"
+            },
+            {
+                height: "100px",
+                width: "200px"
+            }
+        ]
+    return (
+        <div>
+            <h1 className={box[0]}>This is React Css Module Structure</h1>
+           <span> Count No Is:-{count}</span><br />
+           <div className="flex ">
+           <button className="bg-green-500 text-white m-2 p-2 rounded-full
+           " onClick={() => setCount('Vijay Dankhara')}>Increment</button> <br />
+           <button className="bg-red-500 text-white m-2 p-2 rounded-full
+           " onClick={() => setCount('Web Dewlopment')}>Decrement</button><br />
+           <button className="bg-purple-500 text-white m-2 p-2 rounded-full
+           " onClick={() => setCount(initialState)}>Reset</button>
+           </div>
+           <div className="flex ">
+           <button className="bg-green-500 text-white m-2 p-2 rounded-full
+           " onClick={() => setCount(count +1)}>Increment</button> <br />
+           <button className="bg-red-500 text-white m-2 p-2 rounded-full
+           " onClick={() => setCount(count -1)}>Decrement</button><br />
+           <button className="bg-purple-500 text-white m-2 p-2 rounded-full
+           " onClick={() => setCount(initialState)}>Reset</button>
+           </div>
+        </div>
+
+
+
+    )
+}
+
+export default Navbar
