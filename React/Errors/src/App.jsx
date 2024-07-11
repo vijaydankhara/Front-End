2feb601fb6488a9bef3fd36d41b5b18b63e52da9
vijay dankhara <vijaydankhara111@gmail.com>
2024-07-11 09:03:02 +0530

    11-07-2024

---
+++ React/Errors/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Profile from './components/app1'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Profile/>
+      </>
+  )
+}
+
+export default App
---
+++ React/Errors/src/components/app1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/lICfvbD.jpg"
+        alt="Aklilu Lemma"
+      />
+    );
+  }
+  
+export default Profile;
+
+
+// This is Export error so only added export command 
