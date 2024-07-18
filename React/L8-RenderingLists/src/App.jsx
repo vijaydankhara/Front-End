68627c50ea657e7705adcbcce9bfc95c9613cd04
vijay dankhara <vijaydankhara111@gmail.com>
2024-07-18 08:58:27 +0530

    18-07-2024

--- React/L8-RenderingLists/src/App.jsx
+++ React/L8-RenderingLists/src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import List_Rendering from './Components/RenderingLists';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import List_Rendering from "./Components/RenderingLists";
 // import Rendering from './Components/Rendering';
-import RenderingVijay from './Components/RenderinVijay';
+import RenderingVijay from "./Components/RenderinVijay";
+import RenderingCategory from "./Components/RendringByCategory ";
 
 function App() {
   return (
     <>
-    {/* <List_Rendering/> */}
+      {/* <List_Rendering/> */}
       {/* <Rendering /> */}
-<RenderingVijay/>
-
+      {/* <RenderingVijay/> */}
+      <RenderingCategory/>
     </>
   );
 }
